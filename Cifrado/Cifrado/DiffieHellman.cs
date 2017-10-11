ef9767a0f78a48e46699f6f419cffdd2a09e48ea
Diego Moir <diegojmoir@gmail.com>
2017-10-10 23:29:39 -0600

    Implementacion DiffieHellman y Clase Utilities

---
+++ Cifrado/Cifrado/DiffieHellman.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Cifrado
+{
+    class DiffieHellman
+    {
+
+        private int bits = 256;
+        Random generator;
+        Utilities conversions;
+        BigInteger prime;
+
+        BigInteger g;
+
+        BigInteger mine;
+
+        byte[] key;
+
+        string representation;
+
+        public byte[] Key
+        {
+            get { return key; }
+        }
+        public DiffieHellman(int bits)
+        {
+            this.bits = bits;
+            generator = new Random();
+            conversions = new Utilities();
+
+        }
+
+        public DiffieHellman GenerateRequest()
+        {
+
+            prime = (BigInteger)734375557;
+
+            mine = generator.Next(100000000, 734375556);
+            g = (BigInteger)5;
+
+            StringBuilder rep = new StringBuilder();
+            rep.Append(conversions.ConvertToBase(prime, 36));
+            rep.Append("|");
+            rep.Append(conversions.ConvertToBase(g, 36));
+            rep.Append("|");
+
+            BigInteger send = BigInteger.ModPow(g, mine, prime);
+            rep.Append(conversions.ConvertToBase(send, 36));
+
+            representation = rep.ToString();
+            return this;
+        }
+
+        public DiffieHellman GenerateResponse(string request)
+        {
+            string[] parts = request.Split('|');
+
+            BigInteger prime = conversions.ToDecimal(parts[0]);
+            BigInteger g = conversions.ToDecimal(parts[1]);
+            BigInteger mine = generator.Next(1000000, 9599172);
+            BigInteger given = conversions.ToDecimal(parts[2]);
+            BigInteger key = BigInteger.ModPow(given, mine, prime);
+            this.key = key.ToByteArray();
+            this.key = this.key.Reverse().ToArray();
+            BigInteger send = BigInteger.ModPow(g, mine, prime);
+            this.representation = conversions.ConvertToBase(send, 36);
+
+            return this;
+        }
+
+        public void HandleResponse(string response)
+        {
+            BigInteger given = conversions.ToDecimal(response);
+            BigInteger key = BigInteger.ModPow(given, mine, prime);
+            this.key = key.ToByteArray();
+            this.key = this.key.Reverse().ToArray();
+
+        }
+
+        public override string ToString()
+        {
+            return representation;
+        }
+    }
+}
---
+++ Cifrado/Cifrado/Utilities.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+namespace Cifrado
+{
+    class Utilities
+    {
+        String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public String ConvertToBase(BigInteger input, int radix)
+        {
+            if (input < 0) throw new ArgumentOutOfRangeException("input", input, "input cannot be negative");
+
+            char[] clistarr = chars.ToCharArray();
+            var result = new Stack<char>();
+            while (input != 0)
+            {
+                result.Push(clistarr[(int)input % 36]);
+                input /= 36;
+            }
+            return new string(result.ToArray());
+        }
+
+        public BigInteger ToDecimal(string Base36Value)
+        {
+       
+            if (Base36Value == "")
+            {
+                return 0;
+            }
+
+            Base36Value = Base36Value.ToUpper();
+            bool isNegative = false;
+
+            if (Base36Value[0] == '-')
+            {
+                Base36Value = Base36Value.Substring(1);
+                isNegative = true;
+            }
+
+            try
+            {
+                BigInteger returnValue = Base36DigitToNumber(Base36Value[Base36Value.Length - 1]);
+
+                for (int i = 1; i < Base36Value.Length; i++)
+                {
+                    returnValue += ((long)Math.Pow(36, i) * Base36DigitToNumber(Base36Value[Base36Value.Length - (i + 1)]));
+                }
+
+                if (isNegative)
+                {
+                    return returnValue * -1;
+                }
+                else
+                {
+                    return returnValue;
+                }
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        private static byte Base36DigitToNumber(char Base36Digit)
+        {
+            if (!char.IsLetterOrDigit(Base36Digit))
+            {
+                return 0;
+            }
+
+            if (char.IsDigit(Base36Digit))
+            {
+                return byte.Parse(Base36Digit.ToString());
+            }
+            else
+            {
+                return (byte)((int)Base36Digit - 55);
+            }
+        }
+
+
+    }
+}
